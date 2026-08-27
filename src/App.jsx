aa5c8c326bc8bda98b076d3c3c096269e85c0c0c
M HAMMMAS <hammasali498@gmail.com>
2026-08-27 05:33:50 -0700

    Fix React Router base path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
 import NotificationToast from "./components/common/NotificationToast";
-export default function App(){return <BrowserRouter><AppRoutes/><NotificationToast/></BrowserRouter>}
+
+export default function App() {
+  return (
+    <BrowserRouter basename="/Moonlight-Fabrics">
+      <AppRoutes />
+      <NotificationToast />
+    </BrowserRouter>
+  );
+}
